de07a7d96b8ff239aa56ab837e3c5873bf1e0b52
Nilton César <104584538+NiltonCsantos@users.noreply.github.com>
2023-04-27 19:07:04 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
-const Api=axios.create({baseURL: 'https://barbershop-api-2tns.onrender.com'});
+const Api = axios.create({
+  baseURL: "https://barbershop-api-2vva.onrender.com",
+});
 
 export default Api;
